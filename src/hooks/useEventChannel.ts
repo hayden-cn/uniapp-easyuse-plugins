cd9a9ecfc0ae2fc9a01e3346f79946d3a0835198
hayden <48267247+hayden-cn@users.noreply.github.com>
2026-06-23 10:51:00 +0800

    feat: 统一useEventChannel使用参数

--- src/hooks/useEventChannel.ts
+++ src/hooks/useEventChannel.ts
@@ -10,30 +10,32 @@ declare module "vue" {
 export const useEventChannel = (
   effect?: (eventChannel: UniNamespace.EventChannel) => void,
 ): UniNamespace.EventChannel => {
-  const eventChannel = ref<UniNamespace.EventChannel>();
+  const $eventChannel = ref<UniNamespace.EventChannel>();
+
+  const eventChannel = {
+    emit: (eventName: string, data?: any) => {
+      $eventChannel.value?.emit(eventName, data);
+    },
+    on: (eventName: string, callback: (data: any) => void) => {
+      $eventChannel.value?.on(eventName, callback);
+    },
+    off: (eventName: string, callback?: (data: any) => void) => {
+      $eventChannel.value?.off(eventName, callback);
+    },
+    once: (eventName: string, callback: (data: any) => void) => {
+      $eventChannel.value?.once(eventName, callback);
+    },
+  };
 
   onMounted(() => {
     const instance = getCurrentInstance()?.proxy;
-    eventChannel.value = instance?.getOpenerEventChannel();
-    if (eventChannel.value) {
-      effect?.(eventChannel.value);
+    $eventChannel.value = instance?.getOpenerEventChannel();
+    if ($eventChannel.value) {
+      effect?.(eventChannel);
     } else {
       getLoggingInstance().warn("[useEventChannel] 未获取到 eventChannel 实例");
     }
   });
 
-  return {
-    emit: (eventName: string, data?: any) => {
-      eventChannel.value?.emit(eventName, data);
-    },
-    on: (eventName: string, callback: (data: any) => void) => {
-      eventChannel.value?.on(eventName, callback);
-    },
-    off: (eventName: string, callback?: (data: any) => void) => {
-      eventChannel.value?.off(eventName, callback);
-    },
-    once: (eventName: string, callback: (data: any) => void) => {
-      eventChannel.value?.once(eventName, callback);
-    },
-  };
+  return eventChannel;
 };
